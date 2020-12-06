d0ead3cc7faeb365a5265fce564682506e1bc80d
Matthieu Vion <contact@mvion.fr>
2020-12-06 16:45:32 +0100

    ci: putting PDF version of resume in the output

--- scripts/render-resume.js
+++ scripts/render-resume.js
@@ -1,10 +1,14 @@
+const path = require('path');
+
 const puppeteer = require('puppeteer');
+
+const destination = path.normalize(`${__dirname}/../_site/cv.pdf`);
 
 (async () => {  
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto(`http://localhost:5000/cv/`, {waitUntil: 'networkidle2'});
-  await page.pdf({path: 'cv.pdf', format: 'A4', printBackground: true, margin: { bottom: '5px', right: '5px'}});
+  await page.pdf({path: destination, format: 'A4', printBackground: true, margin: { bottom: '5px', right: '5px'}});
 
   await browser.close();
 })();
